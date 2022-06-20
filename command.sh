761fda120d6476b65c9724469fa2280007eba2a6
KaushikTest <kaushik6990@gmail.com>
2022-06-20 17:05:11 +0530

    Refactor

--- command.sh
+++ command.sh
@@ -7,4 +7,8 @@ mkdir dir1 dir2 # creating two directories
 ls #To view the files in the current directory
 cd dir1 #To change the current directory
 cd .. #To change the current directory to the parent directory
-rm -rf dir1 dir2
+rm -rf dir1 dir2 #To delete the directories
+touch samplefile.txt #To create a file
+file samplefile.txt #To view the file details
+rm samplefile.txt #To delete the file
+cat textsample.txt #To view the file contents
