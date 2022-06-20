83b455dd38b2bf93928572d444ff47aba86944aa
KaushikTest <kaushik6990@gmail.com>
2022-06-20 16:04:21 +0530

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+date # date command
+whoami # prints the user name
+pwd # prints the current directory
+mkdir dir1 dir2 # creating two directories
+ls #To view the files in the current directory
+cd dir1 #To change the current directory
+cd .. #To change the current directory to the parent directory
+rm -rf dir1 dir2
