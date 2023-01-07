4155b149fed0f6f41f80169c836169e26ae9e195
KaushikTest <kaushik6990@gmail.com>
2023-01-07 16:59:57 +0530

    Refactor
    added few sed commands

--- command.sh
+++ command.sh
@@ -12,4 +12,26 @@ touch samplefile.txt #To create a file
 file samplefile.txt #To view the file details
 rm samplefile.txt #To delete the file
 cat textsample.txt #To view the file contents
-# less textsample.txt #To view the file
+# less textsample.txt #To view the file 
+# history #To view the history of the commands
+# history -c #To clear the history
+mkdir dir1 dir2
+cd dir1
+touch dir1file.txt
+cd ..
+cp dir1/dir1file.txt dir2
+ls dir2
+ls dir1
+rm -rf dir1 dir2
+#replace a string in a file using sed command and write in new file 
+sed 's\sample\sample1\' textsample.txt > text1sample.txt
+cat text1sample.txt; echo
+rm text1sample.txt
+#add a string at beginning of the line
+sed 's\^\sample1\' textsample.txt > text1sample.txt
+cat text1sample.txt; echo
+rm text1sample.txt
+#add a string at end of the line
+sed 's\$\sample1\' textsample.txt > text1sample.txt
+cat text1sample.txt; echo
+rm text1sample.txt
